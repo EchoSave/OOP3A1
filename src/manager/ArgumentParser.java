b99fa3f62abe9745ef75786828246d997e5f3d92
unknown <nmtri291199@gmail.com>
2026-06-02 09:06:28 +0700

    Completing ArgumentParser, FileLoader, and ShapeFactory

---
+++ src/manager/ArgumentParser.java
@@ -0,0 +1,76 @@
+package manager;
+
+public class ArgumentParser {
+	private String fileName;
+	private char compareType;
+	private char sortType;
+	
+	public ArgumentParser (String[] args) {
+		parse(args);
+		validate();
+	}
+	
+	private void parse(String[] args) {
+		for (String arg : args) {
+			String currentArg = arg.trim().toLowerCase();
+			
+			if (currentArg.startsWith("-f")) {
+				fileName = currentArg.substring(2);
+			} else if (currentArg.startsWith("-t")) {
+				compareType = currentArg.charAt(2);
+			} else if (currentArg.startsWith("-s")) {
+				sortType = currentArg.charAt(2);
+			}
+		}
+	}
+	
+	private void validate() {
+		if (fileName == null || fileName.isEmpty()) { 
+			throw new IllegalArgumentException( "Invalid file path. Use: -fshapes1.txt, -fshapes2.txt, -fshapes3.txt"); 
+		} 
+		
+		if (compareType != 'h' && compareType != 'a' && compareType != 'v') {
+			throw new IllegalArgumentException( "Invalid compare type. Use h, a, or v."); 
+		} 
+		
+		if (sortType != 'b' 
+				&& sortType != 's'
+				&& sortType != 'i' 
+				&& sortType != 'm' 
+				&& sortType != 'q' 
+				&& sortType != 'z') {
+			throw new IllegalArgumentException( "Invalid sort type. Use b, s, i, m, q, or z."); 
+		}
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public char getCompareType() {
+		return compareType;
+	}
+
+	public void setCompareType(char compareType) {
+		this.compareType = compareType;
+	}
+
+	public char getSortType() {
+		return sortType;
+	}
+
+	public void setSortType(char sortType) {
+		this.sortType = sortType;
+	}
+
+	@Override
+	public String toString() {
+		return "ArgumentParser [fileName=" + fileName + ", compareType=" + compareType + ", sortType=" + sortType + "]";
+	}
+	
+	
+}
